84cd1aa02264c99e268a84f04460ff7633314a88
Stephen Ling <lingfromsh@163.com>
2022-02-14 11:04:25 +0800

    Fix test.

--- tests/test_string.py
+++ tests/test_string.py
@@ -6,6 +6,12 @@ from src.types import String
 
 @pytest.fixture
 def UserCache():
+    from redis import StrictRedis
+
+    r_client = StrictRedis(host="127.0.0.1", port=6379, db=0)
+    r_client.set("user:1", "a")
+    r_client.set("user:2", "b")
+    r_client.set("user:3", "c")
     return Key("user:{user_id}", data_type=String())
 
 
@@ -17,7 +23,7 @@ def test_string_type_get(UserCache):
     assert user2 is not None
     assert user3 is not None
     assert user1 == "a"
-    assert user2 == "a"
+    assert user2 == "b"
     assert user3 == "c"
 
 
@@ -25,4 +31,4 @@ def test_string_type_mget(UserCache):
     users = UserCache.mget(
         key_mappings=[{"user_id": 1}, {"user_id": 2}, {"user_id": 3}]
     )
-    assert users == {"user:1": "a", "user:2": "a", "user:3": "c"}
+    assert users == {"user:1": "a", "user:2": "b", "user:3": "c"}
